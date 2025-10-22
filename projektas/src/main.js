9e1152753b8c9e6bbf1ac15751290ac2cb83f83b
KarolisSutas <sutaskarolis@gmail.com>
2025-10-22 18:26:00 +0300

    create: workflow files, start

---
+++ projektas/src/main.js
@@ -0,0 +1,8 @@
+import './main.css';
+import 'flowbite';
+import { initMenu } from './components/menu.js';
+import { initForm } from './components/form.js';
+
+console.log('App start');
+initMenu();
+initForm();
---
+++ projektas/tailwind.config.js
@@ -0,0 +1,5 @@
+export default {
+    content: ["./index.html", "./about.html", "./contact.html", "./src/**/*.{js,html}"],
+    theme: { extend: {} },
+    plugins: [],
+};  
